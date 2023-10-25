55ce3fdf83b467add467e0b5b4d82ddb56cd311f
Olena <petrova1512@gmail.com>
2023-10-25 21:19:04 +0200

    hw done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number = Math.random();
+
+  public getSignatur() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: object) {}
+
+  public getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: object[] = [];
+  protected door: boolean = false;
+
+  public comeIn(person: object) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: object): void;
+}
+
+class MyHouse extends House {
+  constructor(private key: object) {
+    super();
+  }
+
+  public openDoor(personKey: object): void {
+    if (key === personKey) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
